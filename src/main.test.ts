03f46be274391d14bbbf6ea10a23850311ab48c9
IvaTsu <ivan.tsud@gmail.com>
2025-02-24 16:04:45 +0100

    feat: add solution

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,8 +1,28 @@
 import {describe, it, expect} from 'vitest';
 import { main } from "./main";
 
+const inputs = [
+	[2, 3, 4],
+	[1, 2, 0, 4],
+	[3, 0, 4, 0],
+	[-1, 2, -3, 4],
+	[5],
+	[2, 3],
+]
+
+const outputs = [
+	[12, 8, 6],
+	[0, 0, 8, 0],
+	[0, 0, 0, 0],
+	[-24, 12, -8, 6],
+	[1],
+	[3, 2],
+]
+
 describe('main', () => {
-	it('should return "Hello World!"', () => {
-		expect(main()).toBe("Hello World!");
+	inputs.forEach((input, index) => {
+		it(`should return [${outputs[index]}] when input is [${input}]`, () => {
+			expect(main(input)).toEqual(outputs[index]);
+		});
 	});
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
-export function main() {
-	return "Hello World!";
+export function main(input: number[]) {
+	let result: number[] = [];
+	for (let i = 0; i < input.length; i++) {
+		const res = input.reduce((acc, cur) => acc * cur, 1);
+		if (input[i] === 0) {
+			const copy = [...input];
+			copy.splice(i, 1);
+			result.push(copy.reduce((acc, cur) => acc * cur, 1));
+		} else {
+			result.push(res / input[i]);
+		}
+	}
+	return result;
 }
