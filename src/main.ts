09a4bda712191c531388d34ce4f0f5dec83094bb
IvaTsu <ivan.tsud@gmail.com>
2025-02-24 16:14:13 +0100

    hotfix: move totalProduct calculation outside of main loop

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 export function main(input: number[]) {
 	let result: number[] = [];
+	const totalProduct = input.reduce((acc, cur) => acc * cur, 1);
+
 	for (let i = 0; i < input.length; i++) {
-		const res = input.reduce((acc, cur) => acc * cur, 1);
 		if (input[i] === 0) {
 			const copy = [...input];
 			copy.splice(i, 1);
 			result.push(copy.reduce((acc, cur) => acc * cur, 1));
 		} else {
-			result.push(res / input[i]);
+			result.push(totalProduct / input[i]);
 		}
 	}
 	return result;
